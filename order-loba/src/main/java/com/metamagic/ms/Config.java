4692b262b2e44d41a37a20cddabd3107431b8ef9
ashujadhav123 <ashutosh.jadhav@metamagic.in>
2018-04-11 14:29:16 +0530

    order loba

---
+++ order-loba/src/main/java/com/metamagic/ms/Config.java
@@ -0,0 +1,42 @@
+package com.metamagic.ms;
+
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.SimpleClientHttpRequestFactory;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler;
+import org.springframework.web.client.AsyncRestTemplate;
+import org.springframework.web.client.RestTemplate;
+
+@Configuration
+@EnableEurekaClient
+@EnableDiscoveryClient
+@EnableCircuitBreaker
+public class Config {
+
+	@LoadBalanced
+	@Bean
+	public RestTemplate restTemplate(){
+		return new RestTemplate();
+	}
+	
+	@LoadBalanced
+	@Bean
+	public AsyncRestTemplate asyncRestTemplate(@LoadBalanced RestTemplate restTemplate) {
+		SimpleClientHttpRequestFactory requestFactory = new SimpleClientHttpRequestFactory();
+		requestFactory.setTaskExecutor(threadPoolTaskScheduler());
+		return new AsyncRestTemplate(requestFactory, restTemplate);
+	}
+
+	@Bean(destroyMethod = "shutdown")
+	public ThreadPoolTaskScheduler threadPoolTaskScheduler() {
+		ThreadPoolTaskScheduler threadPoolTaskScheduler = new ThreadPoolTaskScheduler();
+		threadPoolTaskScheduler.setPoolSize(10);
+		threadPoolTaskScheduler.initialize();
+		return threadPoolTaskScheduler;
+	}
+
+}
---
+++ order-loba/src/main/java/com/metamagic/ms/OrderLobaApplication.java
@@ -0,0 +1,12 @@
+package com.metamagic.ms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderLobaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrderLobaApplication.class, args);
+	}
+}
