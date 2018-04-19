5f12b3bd56a187c397dbb2a3885dd2c6bf71b18b
sagarjdhv968 <sagar.jadhav@metamagic.in>
2018-04-19 18:37:01 +0530

    Logger added to controller level for Write operation

--- auth-service/src/main/java/com/metamagic/ms/controller/write/UserController.java
+++ auth-service/src/main/java/com/metamagic/ms/controller/write/UserController.java
@@ -1,5 +1,6 @@
 package com.metamagic.ms.controller.write;
 
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
@@ -15,6 +16,8 @@ import com.metamagic.ms.dto.UserDTO;
 import com.metamagic.ms.exception.CustomException;
 import com.metamagic.ms.service.write.UserWriteService;
 
+import ch.qos.logback.classic.Logger;
+
 /**
  * @author sagar
  * 
@@ -23,6 +26,8 @@ import com.metamagic.ms.service.write.UserWriteService;
 @RestController
 @RequestMapping("/user")
 public class UserController {
+	
+	private static final Logger LOGGER = (Logger) LoggerFactory.getLogger(UserController.class);
 
 	@Autowired
 	private UserWriteService userWriteService;
@@ -33,11 +38,14 @@ public class UserController {
 	@PostMapping(value = "/create", produces = MediaType.APPLICATION_JSON_VALUE)
 	public @ResponseBody ResponseEntity<ResponseBean> createUser(@RequestBody UserDTO userDTO){
 		ResponseBean responseBean = null;
+		LOGGER.info("User creation started."+userDTO.getFirstName());
 		try {
 			userWriteService.createUser(userDTO);
 			responseBean = new ResponseBean(true, "User saved successfully.", "success", null);
+			LOGGER.info("User created successfully."+userDTO.getFirstName());
 		} catch (CustomException e) {
 			responseBean = new ResponseBean(false, e.getMessage(), "failure", null);
+			LOGGER.error("User creation failed:"+ e.getMessage());
 		}
 		return new ResponseEntity<ResponseBean>(responseBean, HttpStatus.OK);
 	}
