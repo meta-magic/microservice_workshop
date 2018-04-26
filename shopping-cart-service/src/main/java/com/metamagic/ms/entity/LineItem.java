b92f79546186f1eae26d50cddaffee120a444e79
ashujadhav123 <ashutosh.jadhav@metamagic.in>
2018-04-26 12:32:10 +0530

    Update usercart entity - remove unused methods.

--- shopping-cart-service/src/main/java/com/metamagic/ms/entity/LineItem.java
+++ shopping-cart-service/src/main/java/com/metamagic/ms/entity/LineItem.java
@@ -89,6 +89,29 @@ public class LineItem implements CommonValidation{
 		return "UserProduct [name=" + name + ", quantity=" + quantity + ", price=" + price + "]";
 	}
 
-	
-	
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		LineItem other = (LineItem) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
+
 }
